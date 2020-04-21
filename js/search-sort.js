c49c549c1342ccbc49b7e9bc59799e44899c78ba
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2020-04-21 12:52:36 -0600

    Update search-sort.js

--- js/search-sort.js
+++ js/search-sort.js
@@ -1,30 +1,9 @@
-// Library of Search & Sort Functions
+// Search & Sort Functions Library
 
 function linearSearch(anArray, item) {
-    for (let i = 0; i < anArray.length; i++) {
-        if (item == anArray[i]) {
-            return i;
-        }
-    }
 
-    return -1;
 }
 
 function binarySearch(anArray, item) {
-    let li = 0;
-    let ui = anArray.length - 1;
-    let mi;
 
-    while (li <= ui) {
-        mi = Math.floor((li + ui) / 2);
-        if (item == anArray[mi]) {
-            return mi;
-        } else if (item < anArray[mi]) {
-            ui = mi - 1;
-        } else {
-            li = mi + 1;
-        }
-    }
-
-    return -1;
 }
