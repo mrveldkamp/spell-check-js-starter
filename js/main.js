32396c3a59727ee538a92768360e0b553260e46a
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2020-04-21 13:19:05 -0600

    update instructions

--- js/main.js
+++ js/main.js
@@ -21,11 +21,14 @@ document.getElementById("full-linear-btn").addEventListener("click", fullLinear)
 document.getElementById("full-binary-btn").addEventListener("click", fullBinary);
 
 // Event Functions
+
+// Use a linear search to see if the user's word is in the dictionary array
 function linearWord() {
-  // Get user word and check if it is in the dictionary using a linear search
+  // Get user word and convert to lowercase
   let userWord = document.getElementById("word").value;
   userWord = userWord.toLowerCase();
 
+  // Search dictionary for userWord and output result
   let searchResult = linearSearch(dictionary, userWord);
   if (searchResult == -1) {
     document.getElementById("word-result").innerHTML = "Linear Search: " + userWord + " is NOT in the dictionary.";
@@ -34,11 +37,13 @@ function linearWord() {
   }
 }
 
+// Use a binary search to see if the user's word is in the dictionary array
 function binaryWord() {
-  // Get user word and check if it is in the dictionary using a binary search
+  // Get user word and convert to lowercase
   let userWord = document.getElementById("word").value;
   userWord = userWord.toLowerCase();
 
+  // Search dictionary for userWord and output result
   let searchResult = binarySearch(dictionary, userWord);
   if (searchResult == -1) {
     document.getElementById("word-result").innerHTML = "Binary Search: " + userWord + " is NOT in the dictionary.";
@@ -47,18 +52,42 @@ function binaryWord() {
   }
 }
 
+// CH1 LINEAR FUNCTION CRITERIA
+// Take each word from the Global aliceWordsCh1 array and use a linear search to see if the word is in the Global dictionary array.
+// Output to the console every word that is not found in the dictionary array.
+// Output a total count of words not found in the dictionary in the paragraph with the id 'ch1-result'
 function ch1Linear() {
-
+  // **********************************
+  // * YOUR CODE HERE...
+  // **********************************
 }
 
+// CH1 BINARY FUNCTION CRITERIA
+// Take each word from the Global aliceWordsCh1 array and use a binary search to see if the word is in the Global dictionary array.
+// Output to the console every word that is not found in the dictionary array.
+// Output a total count of words not found in the dictionary in the paragraph with the id 'ch1-result'
 function ch1Binary() {
-
+  // **********************************
+  // * YOUR CODE HERE...
+  // **********************************
 }
 
+// FULL LINEAR FUNCTION CRITERIA
+// Take each word from the Global aliceWordsFull array and use a linear search to see if the word is in the Global dictionary array.
+// Output to the console every word that is not found in the dictionary array.
+// Output a total count of words not found in the dictionary in the paragraph with the id 'full-result'
 function fullLinear() {
-
+  // **********************************
+  // * YOUR CODE HERE...
+  // **********************************
 }
 
+// FULL BINARY FUNCTION CRITERIA
+// Take each word from the Global aliceWordsFull array and use a binary search to see if the word is in the Global dictionary array.
+// Output to the console every word that is not found in the dictionary array.
+// Output a total count of words not found in the dictionary in the paragraph with the id 'full-result'
 function fullBinary() {
-
+  // **********************************
+  // * YOUR CODE HERE...
+  // **********************************
 }
--- js/search-sort.js
+++ js/search-sort.js
@@ -1,9 +1,19 @@
 // Search & Sort Functions Library
 
+// LINEAR SEARCH FUNCTION CRITERIA
+// Search the anArray argument for the item argument using the linear search algorithm.
+// If item found, return index where found, else return -1.
 function linearSearch(anArray, item) {
-
+    // **********************************
+    // * YOUR CODE HERE...
+    // **********************************
 }
 
+// BINARY SEARCH FUNCTION CRITERIA
+// Search anArray for the item using the binary search algorithm.
+// If item found, return index where found, else return -1.
 function binarySearch(anArray, item) {
-
+    // **********************************
+    // * YOUR CODE HERE...
+    // **********************************
 }
